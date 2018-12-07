a3da1a74faf03efa3524f89cab7e5cddd9869552
Aaron Shivers <aaron@moonmail.org>
2018-12-07 13:44:00 -0600

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -26,7 +26,13 @@ app.post('/todos', (req, res) => {
 	})
 })
 
-
+app.get('/todos', (req, res) => {
+	Todo.find().then((todos) => {
+		res.send({todos})
+	}, (err) => {
+		res.status(400).send(err)
+	})
+})
 
 app.listen(port, () => {
 	console.log(`Server listening on port ${port}.`)
