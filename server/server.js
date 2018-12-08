d04f03b85f429e1980ab065c47631bc07f0308b5
Aaron Shivers <aaron@moonmail.org>
2018-12-08 09:24:05 -0600

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 
+const _ = require('lodash')
 const	express = require('express')
 
 const {ObjectId} = require('mongodb')
@@ -47,7 +48,7 @@ app.get('/todos/:id', (req, res) => {
 		if (!todo) {
 			return res.status(404).send('Todo not found')
 		}
-		
+
 		res.send({todo})
 	}).catch((err) => {
 		res.status(400).send('Database Error')
@@ -63,6 +64,33 @@ app.delete('/todos/:id', (req, res) => {
 	}
 	
 	Todo.findByIdAndRemove(id).then((todo) => {
+		if (!todo) {
+			return res.status(404).send('Todo not found')
+		}
+
+		res.send({todo})
+	}).catch((err) => {
+		res.status(400).send('Database Error')
+	})
+})
+
+// Patch Todo
+app.patch('/todos/:id', (req, res) => {
+	const id = req.params.id
+	const body = _.pick(req.body, ['text', 'completed'])
+
+	if (!ObjectId.isValid(id)) {
+		return res.status(404).send('Invalid ObjectId')
+	}
+
+	if (_.isBoolean(body.completed) && body.completed) {
+		body.completedAt = new Date().getTime()
+	} else {
+		body.completed = false
+		body.completedAt = null
+	}
+
+	Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
 		if (!todo) {
 			return res.status(404).send('Todo not found')
 		}
