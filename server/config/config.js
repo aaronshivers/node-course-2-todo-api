e1316942481d23c86bdc3e808784c7be0ce24371
Aaron Shivers <aaron@moonmail.org>
2018-12-11 21:09:20 -0600

    Update config to use json file

--- server/config/config.js
+++ server/config/config.js
@@ -1,12 +1,12 @@
 const env = process.env.NODE_ENV || 'development'
 
 console.log('env *****', env)
-if (env === 'development') {
-  process.env.PORT = 3000
-  process.env.MONGO_DB = process.env.MONGO_DATABASE
-  process.env.MONGO_DB_SERVER = process.env.MONGO_SERVER
-} else if (env === 'test') {
-  process.env.port = 3000
-  process.env.MONGO_DB = process.env.MONGO_DATABASE_TEST
-  process.env.MONGO_DB_SERVER = process.env.MONGO_SERVER_TEST
+
+if (env === 'development' || env === 'test') {
+  const config = require('./config.json')
+  const envConfig = config[env]
+
+  Object.keys(envConfig).forEach((key) => {
+    process.env[key] = envConfig[key]
+  })
 }
--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+// require('dotenv').config()
 require('./config/config')
 
 const  _ = require('lodash')
